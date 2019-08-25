ca59dbd45fac5a87a30e5db830c3d39e9da13ec2
djriffle <djriffle1@gmail.com>
2019-08-25 17:50:11 -0400

    Added pages

--- ui.js
+++ ui.js
@@ -1,4 +1,9 @@
 function moveAbout() {
     window.location.href = "aboutUs/index.html";
-
+}
+function moveRegister(){
+    window.location.href = "https://forms.gle/FNpvwg2RdUNFpR4t8";
+}
+function moveCommittees(){
+    window.location.href = "committees/index.html";
 }
